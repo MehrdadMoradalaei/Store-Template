9b7dffe3d70034c051abd9a02173d81a4c410e89
MehrdadMoradalaei <114387642+MehrdadMoradalaei@users.noreply.github.com>
2023-11-19 21:06:24 +0100

    first-step

---
+++ my-store/src/Billboard.js
@@ -0,0 +1,10 @@
+import './Billboard.css';
+function Billboard(){
+    return(
+        <div className='firstPic'>
+        </div>
+    )
+    
+}
+
+export default Billboard;
---
+++ my-store/src/Nav.js
@@ -0,0 +1,10 @@
+import './Nav.css';
+function Nav(){
+    return(
+    <div className='Line'>
+        <a className='navBtn' href='https://www.google.com/'>Home</a>
+        <a className='navBtn' href='https://www.google.com/'>About</a>
+        <a className='navBtn' href='https://www.google.com/'>Contact</a>
+    </div>)
+}
+export default Nav;
